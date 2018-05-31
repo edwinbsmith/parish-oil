060644325e8be8c00890e35769e9138a63baddce
Edwin Smith <edwin@showit.co>
2018-05-30 20:53:07 -0700

    added header and styles

--- partials/header.php
+++ partials/header.php
@@ -16,13 +16,10 @@ if ( $current_page !== '/' ) {
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<title>Parish Oil</title>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-	<style>
-		ul li .active {
-			background: red;
-		}
-	</style>
+	<link rel="stylesheet" href="assets/styles.css">
 </head>
 <body>
 	<header>
+		<a href=""><img src="http://www.parishoil.com/_/rsrc/1443030341530/config/customLogo.gif?revision=23" alt=""></a>
 		<h1>Parish Oil</h1>
 	</header>
